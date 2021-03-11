b2dd4e958547d84eb493ceeac9717fe55a280745
Kyle Smilon <smilonk@moravian.edu>
2021-03-11 10:50:10 -0500

    fixed

--- SmilonsHealer.java
+++ SmilonsHealer.java
@@ -9,4 +9,36 @@ public class SmilonsHealer extends Project02.People{
         super(nation, tribe, PeopleType.wizard, lifePoints);
         myDescription = "\tSmilons Wizard";
     }
+    public int encounterStrategy(Project02.People otherPerson) {
+        int lifePoints = 0;
+        if (this.getNation() != otherPerson.getNation())
+        {
+            if (otherPerson.getLifePoints() < this.getLifePoints())
+            {
+                if (otherPerson.getType() == PeopleType.warrior) // run away
+                {
+                    lifePoints = -this.getLifePoints();
+                }
+                else // attack a wizard
+                {
+                    lifePoints = (int) (this.getLifePoints()/2);
+                }
+            }
+        }
+        else
+        {
+            if (otherPerson.getLifePoints() < this.getLifePoints()) // heal a friend
+            {
+                lifePoints = (int) (this.getLifePoints() - otherPerson.getLifePoints() / 2);
+            }
+            else
+            {
+                lifePoints = 0;
+            }
+        }
+        return lifePoints;
+    }
+
 }
+
+
--- SmilonsWarrior.java
+++ SmilonsWarrior.java
@@ -9,5 +9,37 @@ public class SmilonsWarrior extends People{
         super(nation, tribe, PeopleType.warrior, lifePoints);
         myDescription = "\tSmilons Warrior";
     }
+    public int encounterStrategy(People otherPerson)
+    {
+        int lifePoints = 0;
+        if(this.getNation() == otherPerson.getNation())
+        {
+            if(otherPerson.getLifePoints() < this.getLifePoints())
+            {
+                if(otherPerson.getTribe() == this.getTribe())
+                {
+                    lifePoints = -((this.getLifePoints() - otherPerson.getLifePoints()) / 2);
+                }
+                else
+                {
+                    lifePoints = -((this.getLifePoints() - otherPerson.getLifePoints()) / 4);
+                }
+            }
+        }
+        else
+        {
+            int points;
+            points = this.getLifePoints() - otherPerson.getLifePoints();
+            if (points > 0)
+            {
+                lifePoints = otherPerson.getLifePoints();
+            }
+            else
+            {
+                lifePoints = this.getLifePoints();
+            }
+        }
+        return lifePoints;
+    }
 
 }
