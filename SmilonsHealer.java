ae242572d55a5e4fbb91d0a042f6320dd1c90f8a
Kyle Smilon <smilonk@moravian.edu>
2021-03-11 10:31:33 -0500

    fixed name errorr

--- SmilonsHealer.java
+++ SmilonsHealer.java
@@ -2,6 +2,6 @@ public class SmilonsHealer {
     SmilonsHealer(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.wizard, lifePoints);
-        myDescription = "\tSchaper Wizard";
+        myDescription = "\tSmilons Wizard";
     }
 }
